57554a57b098be3587dd1b9978c94a2305b2e0f6
Kotatko <vahalova@gmail.com>
2018-02-04 21:04:40 +0100

    vyber her

--- hry.py
+++ hry.py
@@ -4,7 +4,7 @@ from time import sleep
 pin = Pin(15, Pin.OUT)
 pin_up = Pin(0, Pin.IN, Pin.PULL_UP)
 pin_down = Pin(13, Pin.IN, Pin.PULL_UP)
-
+TEXT = ((1,0),(1,1),(1,2),(1,4),(1,5),(1,6),(1,8),(1,9),(1,10),(2,1),(2,4),(2,9),(3,1),(3,4),(3,5),(3,9),(4,1),(4,4),(4,9),(5,1),(5,4),(5,5),(5,6),(5,9),(7,1),(7,3),(7,6),(7,9),(7,10),(8,1),(8,3),(8,5),(8,7),(8,9),(8,11),(9,1),(9,2),(9,3),(9,5),(9,6),(9,7),(9,9),(9,11),(10,1),(10,3),(10,5),(10,7),(10,9),(10,11),(11,1),(11,3),(11,5),(11,7),(11,9),(11,10))
 POCET_LED = 289
 np = NeoPixel(pin, POCET_LED)
 def choice_game():
@@ -21,8 +21,9 @@ def choice_game():
         np.write()
         if pin_down.value()!=0:
             import hadled.py
-for i in range(POCET_LED):
-    np[i]=(0,0,0)
+for row, column in TEXT:
+    number_of_led = row + 2 + column*25
+    np[number_of_led]=(0,5,0)
 np.write()
 
 while True:
